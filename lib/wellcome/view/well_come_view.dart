c0eca50dd38a4220c344ad2f664f456b2f97f96f
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2024-01-02 19:48:42 +0600

    well_come_view has connected to auth_bloc

--- lib/wellcome/view/well_come_view.dart
+++ lib/wellcome/view/well_come_view.dart
@@ -1,8 +1,11 @@
+import 'package:chat_app/app/bloc/auth_bloc.dart';
 import 'package:chat_app/components/components.dart';
 import 'package:chat_app/login/login.dart';
 import 'package:chat_app/register/register.dart';
+import 'package:chat_app/utils/show/app_show.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 class WellComeView extends StatelessWidget {
@@ -14,45 +17,70 @@ class WellComeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Wellcome to Chat App'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset('assets/thunder.svg'),
-                const SizedBox(width: 10.0),
-                const CustomWavyAnimetedText(
-                  text: 'Flash Chat',
-                ),
-              ],
-            ),
-            CustomElevatedButton(
-              text: 'Login',
-              onPressed: () {
-                Navigator.push<void>(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => const LoginView(),
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthenticatedState) {
+            AppShow.navigateHomeUntil(context);
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset('assets/thunder.svg'),
+                  const SizedBox(width: 10.0),
+                  const CustomWavyAnimetedText(
+                    text: 'Flash Chat',
                   ),
-                );
-              },
-            ),
-            const SizedBox(height: 20.0),
-            CustomElevatedButton(
-              text: 'Register',
-              onPressed: () {
-                Navigator.push<void>(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => const RegisterView(),
-                  ),
-                );
-              },
-            ),
-          ],
+                ],
+              ),
+              BlocBuilder<AuthBloc, AuthState>(
+                builder: (context, state) {
+                  if (state is UnauthenticatedState) {
+                    return CustomElevatedButton(
+                      text: 'Login',
+                      onPressed: () {
+                        Navigator.push<void>(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                const LoginView(),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                },
+              ),
+              const SizedBox(height: 20.0),
+              BlocBuilder<AuthBloc, AuthState>(
+                builder: (context, state) {
+                  if (state is UnauthenticatedState) {
+                    return CustomElevatedButton(
+                      text: 'Register',
+                      onPressed: () {
+                        Navigator.push<void>(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                const RegisterView(),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
